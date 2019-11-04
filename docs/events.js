3ee4a2983e4d43f00a28e4df21a6a0f58af61feb
kannokanno <akapanna@gmail.com>
2019-11-04 22:58:24 +0900

    add

--- docs/events.js
+++ docs/events.js
@@ -10,6 +10,6 @@ function getEvents(prefecture) {
 
 function getAllEvents() {
   return [
-    // { prefecture: '', city: '', name: '', message: '', url: '', date: '', startTime: '', endTime: ''},
+    { prefecture: '兵庫', city: '神戸', name: '和田岬一箱古本市', message: '和田岬は神戸市兵庫区の南部にある、昔懐かしい下町です。ここで、はじめての一箱古本市を開催します！和田岬ののんびりした雰囲気と下町情緒をお楽しみください。', url: 'https://twitter.com/wadamisaki2019', date: '11月17日(日)', startTime: '10:00', endTime: '16:00'},
   ];
 }
